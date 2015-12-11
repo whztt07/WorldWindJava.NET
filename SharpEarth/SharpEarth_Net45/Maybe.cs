0000b92c4b6b17bd8c392c3a54d87af8331c600f
James Boyer <james@goerden.com>
2015-12-10 23:37:40 -0500

    Added Maybe class

---
+++ SharpEarth/SharpEarth_Net45/Maybe.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SharpEarth
+{
+  public static class MaybeExtensions
+  {
+    public static Maybe<T> ToMaybe<T>( this T value )
+    {
+      return Maybe<T>.Of( value );
+    }
+  }
+
+  public sealed class Maybe<T> : IEnumerable<T>
+  {
+    public static Maybe<T> Empty = new Maybe<T>();
+    
+    public static Maybe<T> Of( T value )
+    {
+      return new Maybe<T>( value );
+    }
+
+    private readonly IEnumerable<T> value;
+
+    private Maybe()
+    {
+      this.value = new T[0];
+    }
+
+    private Maybe( T value )
+    {
+      this.value = value == null ?
+        new T[0] :
+        new T[1] { value };
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+      return this.value.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      return this.value.GetEnumerator();
+    }
+  }
+}
